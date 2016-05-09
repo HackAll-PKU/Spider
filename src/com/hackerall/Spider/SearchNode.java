17f7b5c55343dacecbcb220f6ce579c4d033bed6
Zac Chan <sunshinecltzac@gmail.com>
2016-05-09 22:26:34 +0800

    添加注释

--- src/com/hackerall/Spider/SearchNode.java
+++ src/com/hackerall/Spider/SearchNode.java
@@ -3,10 +3,23 @@ package com.hackerall.Spider;
 /**
  * Created by ChenLetian on 16/5/9.
  */
+
+/**
+ * 搜索的节点类
+ */
 public class SearchNode {
 
+    /**
+     * 从父亲那里过来的锚里面的内容
+     */
     public String fromLinkerTitle;
+    /**
+     * 节点的url
+     */
     public String url;
+    /**
+     * 节点的父亲所在的索引
+     */
     public int fatherIndex;
 
     public SearchNode(String fromLinkerTitle, String url, int fatherIndex) {
